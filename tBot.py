472e2a47d8d6465c609e60d037ca57a55dee0e11
ald0us <36396275+ald0us@users.noreply.github.com>
2018-02-27 08:25:56 -0500

    Update tBot.py

--- tBot.py
+++ tBot.py
@@ -5,29 +5,31 @@ with open('sdfsdf.json') as json_data:
 
     j = json.load(json_data)
     for i in range(2):
+
         i += 1
         i = str(i)
+
         CONSUMER_KEY = j[i]['CONSUMER_KEY']
         CONSUMER_SECRET = j[i]['CONSUMER_SECRET']
         ACCESS_TOKEN = j[i]['ACCESS_TOKEN']
         ACCESS_SECRET = j[i]['ACCESS_SECRET']
+
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
         api = tweepy.API(auth)
 
-        list_id = '928188552910741505'
+        status_id = 'insert status id to deploy on'
 
-        api.update_status("@usernameToReplyTo this is another shot at this", in_reply_to_status_id = 928188552910741505)
-        print(j[i]['USER'], "updated the status")
+        if j[i]['POST'] == "True":
+            api.update_status("@UserName " + j[i]['COMMENT'], in_reply_to_status_id = status_id)
+            print(j[i]['USER'], "Replied to status")
 
-        api.create_favorite(list_id)
-        print(j[i]['USER'], "favorited the tweet")
+        api.create_favorite(status_id)
+        print(j[i]['USER'], "Favorited Tweet")
 
-        api.retweet(list_id)
-        print(j[i]['USER'], "retweeted the tweet")
+        api.retweet(status_id)
+        print(j[i]['USER'], "Retweeted")
 
         print("Pause for 2 seconds")
         time.sleep(2)
-
-
 
